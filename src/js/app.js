6ad24c992a534299dc4abcc2966a9d98098ba0d7
Ruslan Safin <rusel91@idz.ru>
2018-05-27 13:56:47 +0400

    add contact page

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,22 @@ import $ from 'jquery';
 import 'owl.carousel';
 import remodal from 'remodal';
 
+/* scroll to anchor */
+$(function() {
+  $("a[href*='#']:not([href='#'])").click(function() {
+    if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top - 50
+        }, 500);
+        return false;
+      }
+    }
+  });
+});
+
 /* add and remove class head to scroll top */
 $(window).scroll(function() {
   var scroll = $(window).scrollTop();
